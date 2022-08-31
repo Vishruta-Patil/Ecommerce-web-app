d7fbcd13a37bc98ba8214f27ab00ccb6c13b6207
Vishruta_Patil <vishrutapatil2106@gmail.com>
2022-08-31 17:45:53 +0530

    feat: Contact Page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,3 +1,52 @@
+import "./contact.css";
+import insta from "assets/insta.png";
+import twitter from "assets/twitter.png";
+import facebook from "assets/facebook.png";
+
 export const Contact = () => {
-  return <div>This is Contact Page</div>;
+  const contactData = [
+    {
+      emailId: "support@kicksup.com",
+      description: "for any technical support",
+    },
+
+    {
+      emailId: "info@kicksup.com",
+      description: "for more information",
+    },
+    {
+      emailId: "feedback@kicksup.com",
+      description: "to send your feedback",
+    },
+    {
+      emailId: "jobs@kicksup.com",
+      description: "to work with us",
+    },
+  ];
+  return (
+    <section className="contact-content">
+      <div className="mild-shadow contact-block">
+        <p className="font-md contact-main-header">REACH US AT</p>
+
+        <div>
+          {contactData.map((item, index) => (
+            <div className="contact-unit">
+              <p className="font-sm">{item.emailId}</p>
+              <p className="font-xs">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="social-handles">
+        <p className="font-xs">Stay in touch</p>
+
+        <div className="flex-center social-icons">
+          <img src={twitter} alt="twitter" />
+          <img src={insta} alt="insta" />
+          <img src={facebook} alt="facebook" />
+        </div>
+      </div>
+    </section>
+  );
 };
