156ceb1035e50e362096b6c9cab05fd7b92e40a4
Vishruta_Patil <vishrutapatil2106@gmail.com>
2022-09-01 00:52:53 +0530

    feat: jourmey page

---
+++ src/data/journeyData.js
@@ -0,0 +1,23 @@
+export const journeyData = [
+  {
+    title: "THE ROOTS",
+    description:
+      "Our entire life when we shop, we are limited by choices that are there in the store. We are often on that side where we buy our favourite coloured shoes made by someone else but what if we designed it ? What if we could involve in the making of a shoe we want to buy? This little thought sprouted to become what we are today. KICKSUP! ;)",
+  },
+  {
+    title: "CHAPTER I",
+    description:
+      "Understanding the trends in the industry and what users think when they buy were our top priorities before we moved further in the building. After weeks of fruitful research, we stepped into the user's shoes and look at what we plan to do. It's a great joy for the users if they are invoved in the making. We are heading into an era of technology and prefer online shopping to shopping in an outlet.",
+  },
+  {
+    title: "CHAPTER II",
+    description:
+      "Every milestone begins with a single step and before KICKSUP sprouted, the team manufactured, in small scale with two users a month. This experience was crucial to our brand as this was our first practical experience. Interacting with the users was the best thing ever. The positive reviews and support from people motivated us to push our limits further.",
+  },
+  {
+    title: "KICKSUP",
+    description:
+      "Here we are, made a perfect platform for you to involve in the making of a shoe you want to buy! We have evolved ever since we started out. We wish to help you more and make KICKSUP a bigger team. Design your shoe and be a part of our journey!",
+  },
+];
+
--- src/pages/journey/index.js
+++ src/pages/journey/index.js
@@ -1,7 +1,22 @@
+import grasspic from "assets/grasspic.jpg";
+import { journeyData } from "data/journeyData";
+import "./journey.css";
+
 export const Journey = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <section className="journey-container">
+        <div className="journey-img-box">
+      <img src={grasspic} alt="journey" className="journey-img" />
+      <p className="journey-img-content">THE JOURNEY</p>
+      </div>
+      <div className="journey-content">
+        {journeyData.map((item, index) => (
+          <div className="journey-content-unit" key={index}>
+            <p className="journey-unit-header">{item?.title}</p>
+            <p className="font-xs">{item?.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
